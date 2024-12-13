746ffa0c298e3fd687dfb7425ab3a289e366a1c7
pking23 <pking23@gmu.edu>
2024-12-13 17:49:37 -0500

    created 800x600 JFRAME window

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,22 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+
+        // Create new JFrame
+        JFrame window = new JFrame();
+        window.setTitle("Game Title!");
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
     }
 
 
