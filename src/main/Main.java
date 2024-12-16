8970ab8f462b04efda152534c7e8f5b04d5cf4a7
pkingGMU <pking23@gmu.edu>
2024-12-15 20:23:01 -0500

    imported java awt for Action Events and Listeners

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,11 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
