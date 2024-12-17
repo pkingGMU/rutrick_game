9c3db86ce9d5e3727c178b584b1703158c092453
Ruhana Hasan <xmarinakat101x@gmail.com>
2024-12-17 16:06:07 -0500

    changed frame to init with .getContentPane so it would set the preferred size to the CONTAINER SIZE and not the frame size

--- src/main/WindowSettings.java
+++ src/main/WindowSettings.java
@@ -2,27 +2,47 @@ package main;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
 import javax.swing.JFrame;
 
 public class WindowSettings {
 
-    private JFrame window;
-    private Container contentPane;
+    final private JFrame window;
+    final private Container contentPane;
+    final private Insets insets;
 
     public WindowSettings(String title, int width, int height) {
 
         //Initialize JFrame
         window = new JFrame(title);
 
+       
+        
+
         //Set basic properties
         
-        window.setSize(width, height);
+        //window.setSize(width, height);
         window.setLocationRelativeTo(null);
         window.setLayout(null);
         window.setResizable(false);
         window.getContentPane().setBackground(Color.BLACK);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Insets to guarentee 800x600
+        
+        insets = window.getInsets();
+        int frameWidth = width + insets.left + insets.right;
+        int frameHeight = height + insets.top + insets.bottom;
+
+        System.out.println("insets left");
+        System.out.println(insets.left);
+
+        System.out.println("frame width");
+        System.out.println(frameWidth);
+
+        window.getContentPane().setPreferredSize(new Dimension(width, height));
+        window.pack();
 
         // Store content pane for further customization
         contentPane = window.getContentPane();
@@ -39,6 +59,7 @@ public class WindowSettings {
     }
 
     public void showWindow() {
+        
         window.setVisible(true);
     }
 
@@ -47,4 +68,4 @@ public class WindowSettings {
     }
     
     
-}
+
