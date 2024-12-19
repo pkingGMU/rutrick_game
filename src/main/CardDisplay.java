2f2015f0dc64f20445ccfc4f26f31e6802e04609
Ruhana Hasan <xmarinakat101x@gmail.com>
2024-12-18 19:02:04 -0500

    Added card display that extends jpanel to create a new panel with the cards

---
+++ src/main/CardDisplay.java
@@ -0,0 +1,71 @@
+package main;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+
+
+
+public class CardDisplay extends JPanel{
+    
+    public CardDisplay(ArrayList<Card> cards, CreateObjectSpace handViewSpace) {
+
+        handViewSpace.applyBounds(this);
+
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        this.setBackground(Color.orange);
+
+
+        
+
+        for (Card card : cards) {
+            // Load the card image
+            ImageIcon cardImage = new ImageIcon(card.getImagePath());
+
+            System.out.println(cardImage);
+
+            
+            System.out.println(card.toString());
+
+            // Create a mew button
+            JButton cardButton = new JButton(cardImage);
+            cardButton.setBorder(BorderFactory.createEmptyBorder()); // Remove button border
+            cardButton.setContentAreaFilled(false); // Make background transparent
+            cardButton.setActionCommand(card.toString()); // Attach card details
+            
+
+            this.add(cardButton);
+        }
+
+        
+
+
+
+    }
+
+    // Method to update the panel with new cards
+    public void updateCards(ArrayList<Card> cards) {
+        this.removeAll(); // Clear existing cards
+        for (Card card : cards) {
+            ImageIcon cardImage = new ImageIcon(card.getImagePath());
+            JButton cardButton = new JButton(cardImage);
+            cardButton.setBorder(BorderFactory.createEmptyBorder());
+            cardButton.setContentAreaFilled(false);
+            cardButton.setActionCommand(card.toString());
+            
+            this.add(cardButton);
+        }
+        this.revalidate(); // Refresh the layout
+        this.repaint();
+    }
+
+
+
+
+}
